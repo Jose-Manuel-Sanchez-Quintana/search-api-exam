b2776836274abc4ee030fce472424d61764bf3a3
Jose-Sanchez <josemsanchezq00@gmail.com>
2023-11-03 23:45:30 -0700

    se ha agregado el diseno del front

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Search from './components/search';
 function App() {
   return (
     <div className="bg-gradient-to-r bg-gray-900 h-screen">
-      <h1 className='text-white text-center text-3xl'>Api search - Events</h1>
       <Search />
     </div>
   );
